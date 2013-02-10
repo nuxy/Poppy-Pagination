47e2673574a09013ccf56929a80422e313552887
Marc S. Brooks <marc@mbrooks.info>
2013-02-10 12:46:06 -0800

    Minor changes

--- jquery.poppy.pagination.js
+++ jquery.poppy.pagination.js
@@ -46,10 +46,10 @@
 
 	$.fn.PoppyPagination = function(method) {
 		if (methods[method]) {
-			return methods[method].apply(this, Array.prototype.slice.call(arguments, 1) );
+			return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
 		}
 		else
-		if (typeof method === 'object' || ! method) {
+		if (typeof method === 'object' || !method) {
 			return methods.init.apply(this, arguments);
 		}
 		else {
