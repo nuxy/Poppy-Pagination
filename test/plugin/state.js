a7bc0d3f13911125b8d1935e2c6d986bcf4d04eb
Marc S. Brooks <marc@mbrooks.info>
2014-06-19 12:24:48 -0700

    Added new QUnit assertions to test initial page result element state

---
+++ test/plugin/state.js
@@ -0,0 +1,21 @@
+test('Load State', function() {
+	ok($(page).find('li.last').has('span'), "Result link 'Last' is disabled");
+	ok($(page).find('li.next').has('a'),    "Result link 'Next' is enabled");
+
+	var crumbs = $(page).find('li.crumbs').first().children();
+
+	equal(crumbs.size(), 4, 'Total of 4 pages available');
+
+	ok(crumbs.first().is('span'),     "1st result '1' is disabled");
+	ok(crumbs.find('a:nth-child(1)'), "2nd result '2' is enabled");
+	ok(crumbs.find('a:nth-child(2)'), "3rd result '3' is enabled");
+	ok(crumbs.find('a:nth-child(3)'), "4th result '4' is enabled");
+
+	var options = $(page).find('div.options').first();
+
+	equal(options.find('form').is(':visible'), 0, 'Select menu options are not available');
+
+	equal(options.find('strong:nth-child(1)').text(), 1,  "First result item is '1'");
+	equal(options.find('strong:nth-child(2)').text(), 5,  "Last result item is '5'");
+	equal(options.find('strong:nth-child(3)').text(), 20, "Total items found is '20");
+});
