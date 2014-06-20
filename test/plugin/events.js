78319ac30060706e5d8f22fa18c538f7040a2ae2
Marc S. Brooks <marc@mbrooks.info>
2014-06-19 23:45:08 -0700

    Added test cases to verify 'Next Page/Last Page' link events

---
+++ test/plugin/events.js
@@ -0,0 +1,145 @@
+test('Next Page Event', function() {
+	ok($(page).find('li.next a').trigger('click'), "Click event 'Next Page'");
+
+	stop();
+
+	setTimeout(function() {
+		ok($(page).find('li.last').has('a'), "Result link 'Last Page' is enabled");
+		ok($(page).find('li.next').has('a'), "Result link 'Next Page' is enabled");
+
+		var crumbs1 = $(page).find('li.crumbs').first();
+
+		ok(crumbs1.find('a:nth-child(1)'),    "1st result '1' is enabled");
+		ok(crumbs1.find('span:nth-child(2)'), "2nd result '2' is disabled");
+		ok(crumbs1.find('a:nth-child(2)'),    "3rd result '3' is enabled");
+		ok(crumbs1.find('a:nth-child(3)'),    "4th result '4' is enabled");
+
+		var options1 = $(page).find('div.options').first();
+
+		equal(options1.find('strong:nth-child(1)').text(), 6,  "First result item is '6'");
+		equal(options1.find('strong:nth-child(2)').text(), 10, "Last result item is '10'");
+
+		start();
+
+		ok($(page).find('li.next a').trigger('click'), "Click event 'Next Page'");
+
+		stop();
+
+		setTimeout(function() {
+			ok($(page).find('li.last').has('a'), "Result link 'Last Page' is enabled");
+			ok($(page).find('li.next').has('a'), "Result link 'Next Page' is enabled");
+
+			var crumbs2 = $(page).find('li.crumbs').first();
+
+			ok(crumbs2.find('a:nth-child(1)'),    "1st result '1' is enabled");
+			ok(crumbs2.find('a:nth-child(2)'),    "2nd result '2' is enabled");
+			ok(crumbs2.find('span:nth-child(3)'), "3rd result '3' is disabled");
+			ok(crumbs2.find('a:nth-child(3)'),    "4th result '4' is enabled");
+
+			var options2 = $(page).find('div.options').first();
+
+			equal(options2.find('strong:nth-child(1)').text(), 11, "First result item is '11'");
+			equal(options2.find('strong:nth-child(2)').text(), 15, "Last result item is '15'");
+
+			start();
+
+			ok($(page).find('li.next a').trigger('click'), "Click event 'Next Page'");
+
+			stop();
+
+			setTimeout(function() {
+				ok($(page).find('li.last').has('a'), "Result link 'Last Page' is enabled");
+				ok($(page).find('li.next').has('a'), "Result link 'Next Page' is disabled");
+
+				var crumbs3 = $(page).find('li.crumbs').first();
+
+				ok(crumbs3.find('a:nth-child(1)'),    "1st result '1' is enabled");
+				ok(crumbs3.find('a:nth-child(2)'),    "2nd result '2' is enabled");
+				ok(crumbs3.find('a:nth-child(3)'),    "3rd result '3' is enabled");
+				ok(crumbs3.find('span:nth-child(4)'), "4th result '4' is disabled");
+
+				var options3 = $(page).find('div.options').first();
+
+				equal(options3.find('strong:nth-child(1)').text(), 16, "First result item is '16'");
+				equal(options3.find('strong:nth-child(2)').text(), 20, "Last result item is '20'");
+
+				start();
+			}, 500);
+		}, 500);
+	}, 500);
+});
+
+test('Last Page Event', function() {
+	ok($(page).find('li.last a').trigger('click'), "Click event 'Last Page'");
+
+	stop();
+
+	setTimeout(function() {
+		ok($(page).find('li.last').has('a'), "Result link 'Last Page' is enabled");
+		ok($(page).find('li.next').has('a'), "Result link 'Next Page' is enabled");
+
+		var crumbs1 = $(page).find('li.crumbs').first();
+
+		ok(crumbs1.find('a:nth-child(1)'),    "1st result '1' is enabled");
+		ok(crumbs1.find('a:nth-child(2)'),    "2nd result '2' is enabled");
+		ok(crumbs1.find('span:nth-child(3)'), "3rd result '3' is disabled");
+		ok(crumbs1.find('a:nth-child(3)'),    "4th result '4' is enabled");
+
+		var options1 = $(page).find('div.options').first();
+
+		equal(options1.find('strong:nth-child(1)').text(), 11, "First result item is '11'");
+		equal(options1.find('strong:nth-child(2)').text(), 15, "Last result item is '15'");
+
+		start();
+
+		ok($(page).find('li.last a').trigger('click'), "Click event 'Last Page'");
+
+		stop();
+
+		setTimeout(function() {
+			ok($(page).find('li.last').has('a'), "Result link 'Last Page' is enabled");
+			ok($(page).find('li.next').has('a'), "Result link 'Next Page' is enabled");
+
+			var crumbs2 = $(page).find('li.crumbs').first();
+
+			ok(crumbs2.find('a:nth-child(1)'),    "1st result '1' is enabled");
+			ok(crumbs2.find('span:nth-child(2)'), "2nd result '2' is disabled");
+			ok(crumbs2.find('a:nth-child(2)'),    "3rd result '3' is enabled");
+			ok(crumbs2.find('a:nth-child(3)'),    "4th result '4' is enabled");
+
+			var options2 = $(page).find('div.options').first();
+
+			equal(options2.find('strong:nth-child(1)').text(),  6, "First result item is '6'");
+			equal(options2.find('strong:nth-child(2)').text(), 10, "Last result item is '10'");
+
+			start();
+
+			ok($(page).find('li.last a').trigger('click'), "Click event 'Last Page'");
+
+			stop();
+
+			setTimeout(function() {
+				ok($(page).find('li.last').has('a'), "Result link 'Last Page' is disabled");
+				ok($(page).find('li.next').has('a'), "Result link 'Next Page' is enabled");
+
+				var crumbs3 = $(page).find('li.crumbs').first();
+
+				ok(crumbs3.find('span:nth-child(1)'), "1st result '1' is disabled");
+				ok(crumbs3.find('a:nth-child(1)'),    "2nd result '2' is enabled");
+				ok(crumbs3.find('a:nth-child(2)'),    "3rd result '3' is enabled");
+				ok(crumbs3.find('a:nth-child(3)'),    "4th result '4' is enabled");
+
+				var options3 = $(page).find('div.options').first();
+
+				equal(options3.find('strong:nth-child(1)').text(), 1, "First result item is '1'");
+				equal(options3.find('strong:nth-child(2)').text(), 5, "Last result item is '5'");
+
+				start();
+			}, 500);
+		}, 500);
+	}, 500);
+});
+
+/*test('Breadcrumb Events', function() {
+	ok(true);
+});*/
