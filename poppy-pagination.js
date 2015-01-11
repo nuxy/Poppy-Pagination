a03fb049c5373221cd67c4db51a3b9aa58cb5b72
Marc S. Brooks <marc@mbrooks.info>
2015-01-10 16:15:43 -0800

    Converted inner functions to _ prefixed (assumes private) class methods
    Refactored code
    
    Added new and updated existing JSDoc markup in comments

--- poppy-pagination.js
+++ poppy-pagination.js
@@ -15,11 +15,23 @@ if (!window.jQuery || (window.jQuery && window.jQuery.fn.jquery < '1.8.3')) {
 }
 
 (function($) {
-	var lang = {};
 
+	/**
+	 * @namespace PoppyPagination
+	 */
 	var methods = {
+
+		/**
+		 * Create new instance of Poppy-Pagination
+		 * @memberof PoppyPagination
+		 * @method init
+		 * @param {Object} config
+		 * @param {Function} callback
+		 * @returns {Object} jQuery object
+		 */
 		"init": function(config, callback) {
-			var $this = $(this);
+			var $this = $(this),
+				data  = $this.data();
 
 			// config defaults
 			config = $.extend(true, {
@@ -34,38 +46,278 @@ if (!window.jQuery || (window.jQuery && window.jQuery.fn.jquery < '1.8.3')) {
 				}
 			}, config);
 
-			lang = config.uiText;
+			if ( $.isEmptyObject(data) ) {
+				$this.data({
+					config: config
+				});
+			}
+
+			return $this.PoppyPagination('_createPageResults', callback);
+		},
+
+		/**
+		 * Perform cleanup
+		 * @memberof PoppyPagination
+		 * @method destroy
+		 */
+		"destroy": function() {
+			$(this).removeData();
+		},
+
+		/**
+		 * Perform cleanup
+		 * @memberof PoppyPagination
+		 * @method _createPageResults
+		 * @param {Function} callback
+		 * @returns {Object}
+		 * @private
+		 */
+		"_createPageResults": function(callback) {
+			var $this = $(this),
+				block = $this.parent();
 
 			// if results are available, create page elements
-			var res = calcPageResults(config);
-			if (res.total > 0) {
-				var node = $this.find('div.poppy_pagination');
+			var results = $this.PoppyPagination('_calcPageResults');
+			if (results.total > 0) {
+				var pager = block.find('div.poppy_pagination');
 
 				// remove existing
-				if (node[0]) {
-					node.remove();
+				if (pager[0]) {
+					pager.remove();
 				}
 
 				// .. header
 				var header = $('<div></div>')
 					.addClass('poppy_pagination');
 
-				var block1 = createResultElm(res, callback),
-					block2 = createPagerElm (res, callback);
-				header.append(block2, block1);
-
-				$this.prepend(header);
+				var elm1 = $this.PoppyPagination('_createResultElm', results, callback),
+					elm2 = $this.PoppyPagination('_createPagerElm',  results, callback);
+				header.append(elm2, elm1);
+				block.prepend(header);
 
 				// .. footer
 				var footer = header.clone(true);
 				footer.append(footer.children().get().reverse());
-
-				$this.append(footer);
+				block.append(footer);
 			}
+
+			return block;
 		},
 
-		"destroy": function() {
-			$(this).removeData();
+		/**
+		 * Create page result elements
+		 * @memberof PoppyPagination
+		 * @method _createResultElm
+		 * @param {Object} results
+		 * @param {Function} callback
+		 * @returns {Object}
+		 * @private
+		 */
+		"_createResultElm": function(results, callback) {
+			var $this = $(this),
+				data  = $this.data(),
+				lang  = data.config.uiText;
+
+			// create result total elements
+			var span = $('<span></span>')
+				.append(replaceTokens(lang.RESULTS, results));
+
+			var form = $('<form></form>');
+
+			if (results.total > 10 && results.limit > 1) {
+
+				// .. select menu
+				var label = $('<label></label>')
+					.append(replaceTokens(lang.VIEWING, results));
+
+				form.append(label);
+
+				var option = $('<option>-</option>'),
+					select = $('<select></select>')
+					.append(option);
+
+				var opts = [ 10, 20, 30, 40, 50 ];
+
+				// .. options
+				$.each(opts, function() {
+					if (this <= results.total) {
+						option = $('<option>' + this + '</option>')
+							.attr('value', this);
+						select.append(option);
+
+						// if the option is selected
+						if (this == results.limit) {
+							select.selectedIndex = this;
+						}
+					}
+				});
+
+				// attach menu options events
+				if ( $.isFunction(callback) ) {
+					select.on('change', function() {
+						results.limit = parseInt(this.value);
+						callback(results);
+					});
+				}
+
+				form.append(select);
+			}
+
+			// return HTML object
+			return $('<div></div>')
+				.addClass('options')
+				.append(span, form);
+		},
+
+		/**
+		 * Create the first/last and subsequent page links
+		 * @memberof PoppyPagination
+		 * @method _createPagerElm
+		 * @param {Object} results
+		 * @param {Function} callback
+		 * @returns {Object}
+		 * @private
+		 */
+		"_createPagerElm": function(results, callback) {
+			var $this = $(this),
+				data  = $this.data(),
+				lang  = data.config.uiText;
+
+			// create node elements
+			var list = $('<ul></ul>')
+				.addClass('pages');
+
+			var crumbs = $('<li></li>')
+				.addClass('crumbs');
+
+			// define onclick event
+			function onClickEvent(event) {
+				event.preventDefault();
+				results.start = event.data;
+
+				if ( $.isFunction(callback) ) {
+					callback(results);
+				}
+			}
+
+			// always show 10 results, if available
+			(function() {
+				var show_links = 10,
+					first_link = (results.first / results.limit) - (show_links / 2),
+					last_link  = (results.last  / results.limit) + (show_links / 2);
+
+				for (var i = 1; i <= results.pages; i++) {
+					if (first_link > i) continue;
+					if (last_link  < i) continue;
+
+					var link = null;
+
+					// .. page links
+					if (results.start != i) {
+
+						// bind mouse event
+						link = $('<a>' + i + '</a>')
+							.on('click', i, onClickEvent);
+					}
+					else {
+
+						// disable crumb
+						link = $('<span>' + i + '</span>');
+					}
+
+					crumbs.append(link);
+				}
+			})();
+
+			// .. last page link
+			(function() {
+				if (results.total > 0) {
+					var item = $('<li></li>')
+						.addClass('last');
+
+					var link = null;
+
+					if (results.start > 1) {
+
+						// bind mouse event
+						link = $('<a></a>')
+							.on('click', (results.start - 1), onClickEvent);
+					}
+					else {
+
+						// disable button
+						link = $('<span></span>');
+					}
+
+					link.append(replaceTokens(lang.LAST_PAGE, results));
+					item.append(link);
+					list.append(item);
+				}
+			})();
+
+			// .. page crumbs (links)
+			if (crumbs.children()) {
+				if (results.pages == 1) {
+
+					// TODO: to be removed - use CSS instead
+					crumbs.css('visibility', 'hidden');
+				}
+
+				list.append(crumbs);
+			}
+
+			// .. next page link
+			(function() {
+				if (results.total > 1) {
+					var item = $('<li></li>')
+						.addClass('next');
+
+					var link = null;
+
+					if (results.pages != results.start) {
+
+						// bind mouse event
+						link = $('<a></a>')
+							.on('click', (results.start + 1), onClickEvent);
+					}
+					else {
+
+						// disable button
+						link = $('<span></span>');
+					}
+
+					link.append(replaceTokens(lang.NEXT_PAGE, results));
+					item.append(link);
+					list.append(item);
+				}
+			})();
+
+			if (results.pages == 1) {
+				list.addClass('disabled');
+			}
+
+			return list;
+		},
+
+		/**
+		 * Calculate page totals and return object of results
+		 * @memberof PoppyPagination
+		 * @method _calcPageResults
+		 * @returns {Object}
+		 * @private
+		 */
+		"_calcPageResults": function() {
+			var $this = $(this),
+				data  = $this.data();
+
+			var obj = {};
+			obj.total = data.config.totalResults;
+			obj.limit = data.config.perPage;
+			obj.start = data.config.startPage || 1;
+			obj.pages = getTotalRows(obj.total, obj.limit);
+			obj.first = obj.start * obj.limit + 1 - obj.limit;
+			obj.last  = ((obj.first + obj.limit - 1) < obj.total) ? (obj.first + obj.limit - 1) : obj.total;
+			return obj;
 		}
 	};
 
@@ -83,208 +335,12 @@ if (!window.jQuery || (window.jQuery && window.jQuery.fn.jquery < '1.8.3')) {
 	};
 
 	/**
-	 * Create page result elements
-	 * @param {Object} data
-	 * @param {Function} callback
-	 * @returns {Object}
-	 */
-	function createResultElm(data, callback) {
-
-		// create result total elements
-		var span = $('<span></span>')
-			.append(replaceTokens(lang.RESULTS, data));
-
-		var form = $('<form></form>');
-
-		if (data.total > 10 && data.limit > 1) {
-
-			// .. select menu
-			var label = $('<label></label>').append(replaceTokens(lang.VIEWING, data));
-			form.append(label);
-
-			var option = $('<option>-</option>'),
-				select = $('<select></select>')
-				.append(option);
-
-			var opts = [ 10, 20, 30, 40, 50 ];
-
-			// .. options
-			$.each(opts, function() {
-				if (this <= data.total) {
-					option = $('<option>' + this + '</option>')
-						.attr('value', this);
-					select.append(option);
-
-					// if the option is selected
-					if (this == data.limit) {
-						select.selectedIndex = this;
-					}
-				}
-			});
-
-			// attach menu options events
-			if ( $.isFunction(callback) ) {
-				select.on('change', function() {
-					data.limit = parseInt(this.value);
-					callback(data);
-				});
-			}
-
-			form.append(select);
-		}
-
-		// return HTML object
-		return $('<div></div>')
-			.addClass('options')
-			.append(span, form);
-	}
-
-	/**
-	 * Create the first/last and subsequent page links
-	 * @param {Object} data
-	 * @param {Function} callback
-	 * @returns {Object}
-	 */
-	function createPagerElm(data, callback) {
-
-		// create node elements
-		var list = $('<ul></ul>')
-			.addClass('pages');
-
-		var tmp = $('<li></li>')
-			.addClass('crumbs');
-
-		// define onclick event
-		function onClickEvent(event) {
-			event.preventDefault();
-			data.start = event.data;
-
-			if ( $.isFunction(callback) ) {
-				callback(data);
-			}
-		}
-
-		// always show 10 results, if available
-		(function() {
-			var show_links = 10,
-				first_link = (data.first / data.limit) - (show_links / 2),
-				last_link  = (data.last  / data.limit) + (show_links / 2);
-
-			for (var i = 1; i <= data.pages; i++) {
-				if (first_link > i) continue;
-				if (last_link  < i) continue;
-
-				var link = null;
-
-				// .. page links
-				if (data.start != i) {
-
-					// bind mouse event
-					link = $('<a>' + i + '</a>')
-						.on('click', i, onClickEvent);
-				}
-				else {
-
-					// disable crumb
-					link = $('<span>' + i + '</span>');
-				}
-
-				tmp.append(link);
-			}
-		})();
-
-		// .. last page link
-		(function() {
-			if (data.total > 0) {
-				var item = $('<li></li>')
-					.addClass('last');
-
-				var link = null;
-
-				if (data.start > 1) {
-
-					// bind mouse event
-					link = $('<a></a>')
-						.on('click', (data.start - 1), onClickEvent);
-				}
-				else {
-
-					// disable button
-					link = $('<span></span>');
-				}
-
-				link.append(replaceTokens(lang.LAST_PAGE, data));
-				item.append(link);
-				list.append(item);
-			}
-		})();
-
-		// .. page crumbs (links)
-		if (tmp.children()) {
-			list.append(tmp);
-
-			if (data.pages == 1) {
-
-				// TODO: to be removed - use CSS instead
-				tmp.css('visibility', 'hidden');
-			}
-		}
-
-		// .. next page link
-		(function() {
-			if (data.total > 1) {
-				var item = $('<li></li>')
-					.addClass('next');
-
-				var link = null;
-
-				if (data.pages != data.start) {
-
-					// bind mouse event
-					link = $('<a></a>')
-						.on('click', (data.start + 1), onClickEvent);
-				}
-				else {
-
-					// disable button
-					link = $('<span></span>');
-				}
-
-				link.append(replaceTokens(lang.NEXT_PAGE, data));
-				item.append(link);
-				list.append(item);
-			}
-		})();
-
-		if (data.pages == 1) {
-			list.addClass('disabled');
-		}
-
-		return list;
-	}
-
-	/**
-	 * Calculate page totals and return object of results
-	 * @param {Object} settings
-	 * @returns {Object}
-	 */
-	function calcPageResults(settings) {
-		var obj = {};
-		obj.total = settings.totalResults;
-		obj.limit = settings.perPage;
-		obj.start = settings.startPage || 1;
-		obj.pages = getTotalRows(obj.total, obj.limit);
-		obj.first = obj.start * obj.limit + 1 - obj.limit;
-		obj.last  = ((obj.first + obj.limit - 1) < obj.total) ? (obj.first + obj.limit - 1) : obj.total;
-		return obj;
-	}
-
-	/**
-	 * Return the row total
-	 * @param {String} total
-	 * @param {Number} limit
-	 * @returns {Number}
-	 */
+	* Return the row total
+	* @param {String} total
+	* @param {Number} limit
+	* @returns {Number}
+	* @protected
+	*/
 	function getTotalRows(total, limit) {
 		var count = Math.round(total / limit);
 		if ((count * limit) < total) {
@@ -293,13 +349,13 @@ if (!window.jQuery || (window.jQuery && window.jQuery.fn.jquery < '1.8.3')) {
 		return count;
 	}
 
-
 	/**
-	 * Replace string tokens with result object value
-	 * @param {String} text
-	 * @param {Object} vals
-	 * @returns {String}
-	 */
+	* Replace language string tokens with its object defined value
+	* @param {String} text
+	* @param {Object} vals
+	* @returns {String}
+	* @protected
+	*/
 	function replaceTokens(text, vals) {
 		for (var key in vals) {
 			if (vals[key]) {
